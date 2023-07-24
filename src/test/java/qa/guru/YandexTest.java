777413beaac878d9b03852bd8ddac4f70b176ede
ameryakubov <alex_meryakubov@mail.ru>
2023-07-24 14:12:02 +0300

    added //TODO

--- src/test/java/qa/guru/YandexTest.java
+++ src/test/java/qa/guru/YandexTest.java
@@ -1,13 +1,9 @@
 package qa.guru;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 
 public class YandexTest {
 
@@ -21,8 +17,6 @@ public class YandexTest {
 
     @Test
     void yandexTest() {
-        Selenide.open("https://yandex.ru");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        //TO DO IMPLEMENT!!!
     }
 }
